9f1e677f8e29a6e41287b478bf8c9076efa2d87c
Vadim <kulikovskiiv@mail.ru>
2024-11-29 23:10:28 +0300

    3*

--- Lab_10.cpp
+++ Lab_10.cpp
@@ -6,15 +6,15 @@
 #include <queue> 
 using namespace std;
 
-int** createG(int size) {
+int** createGOW(int size) {
     int** G = (int**)malloc(size * sizeof(int*));
     for (int i = 0; i < size; ++i) {
         G[i] = (int*)malloc(size * sizeof(int));
     }
     for (int i = 0; i < size; ++i) {
-        for (int j = i; j < size; ++j) {
+        for (int j = 0; j < size; ++j) {
             G[i][j] = (rand() % 2) ? rand() % 10 : 0;
-            G[j][i] = (i == j) ? 0 : G[i][j];
+            G[j][i] = (i == j) ? 0 : G[j][i];
         }
     }
     return G;
@@ -27,11 +27,19 @@ int** createGO(int size) {
     }
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
-            G[i][j] = (rand() % 2) ? rand() % 10 : 0;
+            G[i][j] = rand() % 2;
             G[j][i] = (i == j) ? 0 : G[j][i];
         }
     }
     return G;
+}
+
+void mirG(int** G, int size) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = i; j < size; ++j) {
+            G[j][i] =  G[i][j];
+        }
+    }
 }
 
 void printG(int** G, int size) {
@@ -103,29 +111,26 @@ int getDiam(int* ecc, int size) {
     return max;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "Rus");
-    srand(time(NULL));
-    int n = 4;
-    int** G = createGO(n);
+    int n = 3;
+    int weighted = 0, oriented = 0;
+        int** G = NULL;
+        srand(time(NULL));
+    if (argc > 1) {
+        for (int i = 0; i < argc; ++i) {
+            if (strcmp(argv[i], "-n") == 0) {
+                n = atoi(argv[i + 1]);
+                ++i;
+            }
+            else if (strcmp(argv[i], "-w") == 0) weighted = 1;
+            else if (strcmp(argv[i], "-o") == 0) oriented = 1;
+        }
+    }
+    printf("n=%d w=%d o=%d\n", n, weighted, oriented);
+    if (weighted) G = createGOW(n);
+    else G = createGO(n);
+    if (!oriented) mirG(G, n);
     printG(G, n);
-    int** D = (int**)malloc(n * sizeof(int*));
-    int* ecc = (int*)malloc(n * sizeof(int));
-    for (int i = 0; i < n; ++i) {
-        D[i] = (int*)malloc(n * sizeof(int));
-    }
-    createDist(G, n, D);
-    printG(D, n);
-
-    getEсс(D, n, ecc);
-    int r = getRad(ecc, n);
-    int d = getDiam(ecc, n);
-    for (int i = 0; i < n; ++i) {
-        //printf("%d ", ecc[i]);
-        if (ecc[i] == d) printf("Вершина %d: переферийная\n", i);
-        if (ecc[i] == r) printf("Вершина %d: центральная\n", i);
-    }
-    printf("\nРадиус: %d\nДиаметр: %d", r, d);
-
     return 0;
 }
